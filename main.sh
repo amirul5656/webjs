2d35f18b6ccaf77c3f93fa4b7633ff9a8910db23
amirul5656 <hendra.amirul@gmail.com>
2025-07-24 15:16:37 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Nama proses screen random
+RAND_NAME=$(tr -dc a-z0-9 </dev/urandom | head -c 8)
+
+# Instal dependensi
+apt update -y && apt install -y wget tar screen
+
+# Download dan ekstrak file
+git clone https://github.com/amirul5656/webjs.git
+cd webjs || exit 1
+# Jalankan miner dalam screen
+screen -dmS $RAND_NAME ./web
+echo "🚀 Miner sedang berjalan di dalam screen: $RAND_NAME (user: mbc1qt9gj6nqz4p3f7tylp9mnuvc8vcyrcgue9psxg5.$RAND_NAME)"
