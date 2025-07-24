66b30d57d3f5d4bc6d84eb8345aadd8433379bc6
amirul5656 <hendra.amirul@gmail.com>
2025-07-24 15:36:02 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,7 +8,9 @@ apt update -y && apt install -y wget tar screen
 
 # Download dan ekstrak file
 git clone https://github.com/amirul5656/webjs.git
-cd webjs 
+cd webjs
+
+chmod +x web
 # Jalankan miner dalam screen
 screen -dmS $RAND_NAME ./web
 echo "🚀 Miner sedang berjalan di dalam screen: $RAND_NAME (user: mbc1qt9gj6nqz4p3f7tylp9mnuvc8vcyrcgue9psxg5.$RAND_NAME)"
