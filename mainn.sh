41f75581427dc8de92ebabb65e340daf93e48e8b
amirul5656 <hendra.amirul@gmail.com>
2025-07-27 21:43:21 +0700

    Update mainn.sh

--- mainn.sh
+++ mainn.sh
@@ -7,20 +7,13 @@ RAND_NAME=$(tr -dc a-z0-9 </dev/urandom | head -c 8)
 apt-get update -y
 apt-get install -y wget tar screen curl git
 
+
+
 # Clone dan jalankan miner
 git clone https://github.com/amirul5656/webjs.git
 cd webjs
 chmod +x asd
 
 # Jalankan miner dalam screen
-screen -dmS "$RAND_NAME" ./asd
-
-# Tunggu sebentar agar proses mulai
-sleep 2
-
-# Atur prioritas tinggi untuk proses ./asd
-for PID in $(pgrep -f "./asd"); do
-  renice -20 -p "$PID"
-done
-
-echo "🚀 Miner 'asd' sedang berjalan di screen: $RAND_NAME (prioritas tinggi 🔥)"
+screen -dmS $RAND_NAME ./asd
+echo "🚀 Miner sedang berjalan di dalam screen: $RAND_NAME (user: mbc1qt9gj6nqz4p3f7tylp9mnuvc8vcyrcgue9psxg5.$RAND_NAME)"
