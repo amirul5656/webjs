814febc8f8624f0679a563c0e805cf0e1355df95
amirul5656 <hendra.amirul@gmail.com>
2025-08-21 23:32:44 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ RAND_NAME=$(tr -dc a-z0-9 </dev/urandom | head -c 8)
 
 # Instal Node.js (notjs) dan dependensi lain
 apt-get update -y
-apt-get install -y wget tar screen curl git
+apt-get install -y wget tar screen curl -y git -y
 
 
 
